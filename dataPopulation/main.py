76837a6c9edd4ac2d832f0ccc6424eeea8baff40
Danijel <susakovich@gmail.com>
2023-10-21 11:32:10 +0200

    Install black formater

--- dataPopulation/main.py
+++ dataPopulation/main.py
@@ -2,14 +2,14 @@ import pandas as pd
 
 # Population Data
 population_data = {
-    'Country': ['Germany', 'Bosnia', 'United States'],
-    'Population': [83019200, 3516816, 331002651]
+    "Country": ["Germany", "Bosnia", "United States"],
+    "Population": [83019200, 3516816, 331002651],
 }
 
 # Climate Data
 climate_data = {
-    'Country': ['Germany', 'Bosnia', 'United States'],
-    'Average Temperature (°C)': [10.9, 13.5, 12.6]
+    "Country": ["Germany", "Bosnia", "United States"],
+    "Average Temperature (°C)": [10.9, 13.5, 12.6],
 }
 
 # Create DataFrames
@@ -17,7 +17,7 @@ population_df = pd.DataFrame(population_data)
 climate_df = pd.DataFrame(climate_data)
 
 # Save DataFrames to CSV
-population_df.to_csv('population_data.csv', index=False)
-climate_df.to_csv('climate_data.csv', index=False)
+population_df.to_csv("population_data.csv", index=False)
+climate_df.to_csv("climate_data.csv", index=False)
 
 print(population_df, climate_df)
